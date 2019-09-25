c63c589cdd1d49acc90b70ffc6681fc23075fdb4
Christopher R. Simpson <crsimpson@crimson.ua.edu>
2019-09-25 11:57:10 -0500

    Added atmospheric function file to determine speed of a/c

--- launch_window.py
+++ launch_window.py
@@ -3,6 +3,7 @@
 """Predict Orbit Based On Tx Beacon at Known/Fixed Interval"""
 import numpy as np
 import astropy.units as u
+u.imperial.enable()
 from astropy.coordinates import EarthLocation, SkyCoord, AltAz, GCRS, ICRS
 from astropy.time import Time, TimeDelta
 from poliastro.bodies import Earth
@@ -11,6 +12,7 @@ from poliastro.twobody.propagation import mean_motion
 #custom
 import readtle
 from downloadfileurl import download_file
+from atmospherefunction import atmConditions
 
 """Python 3.7
    EH Group, Inc. (c) 2019
@@ -41,11 +43,22 @@ t_del_nounit = np.linspace(0,86400,num=86401)*u.second
 #topocentric frame at Hardaway Hall
 frame_now_hh = AltAz(obstime=t_ob+delta_t,location=loc_hh)
 
+#Velocity of Earth at equator
+Veq = 464.5 *(u.m/u.s)
 #Earth inertial velocity addition to launch
-Vgs_lat = 464.5*np.cos(np.deg2rad(33.213))*(u.m/u.s)
+Vgs_lat = Veq*np.cos(np.deg2rad(33.213))*(u.m/u.s)
 #aircraft speed
-V_ac = np.linspace(0.3,1.6,num=14)#mach number
-"""Import atmosphere function to calculate airspeed"""
-h_ac = 45000*u.ft
+V_ac = np.linspace(0.3,1.6,num=14) * u.one #mach number
+h_ac = 45000*u.imperial.ft
+Temp, press, density, R_air = atmConditions(h_ac)
+speed_of_sound = np.sqrt(1.4*R_air*Temp)
+V_ac = speed_of_sound*V_ac
 #speed of satellite immediately after launch
 V_sat = V_ac + (7800*u.m/u.s)
+#inertial launch azimuth
+beta_i = np.arcsin(np.cos(np.deg2rad(coe[2,0]))/np.cos(np.deg2rad(33.213)))
+#correction to account for Earth rotation velocity addition
+gam_correction = np.arctan2(((Vgs_lat*np.cos(beta_i))+V_ac)/(V_sat - (Veq*np.cos(np.deg2rad(coe[2,0])))))
+#launch azimuth (ascending node)
+beta = beta_i + gam_correction
+
