0255eb9d06050a9a5ebafce1b3f5ee479acc1039
Christopher R. Simpson <crsimpson@crimson.ua.edu>
2019-10-09 03:16:47 -0500

    Added calculations for a staged launch. Changed launch window to only consider DV necessary to achieve

--- launch_window.py
+++ launch_window.py
@@ -4,79 +4,63 @@
 import numpy as np
 import astropy.units as u
 u.imperial.enable()
-from astropy.coordinates import EarthLocation, SkyCoord, AltAz, GCRS, ICRS
-from astropy.time import Time, TimeDelta
+from astropy.coordinates import EarthLocation
 from poliastro.bodies import Earth
 from poliastro.twobody import Orbit
-from poliastro.twobody.propagation import mean_motion
-from poliastro.core.angles import D_to_nu
-from poliastro.coordinates import Longitude
+from poliastro.core.angles import fp_angle
+import matplotlib.pyplot as plt
+#from poliastro.coordinates import Longitude
 #custom
-import readtle
-from downloadfileurl import download_file
+#from staged_launch import aim120ToSpace
 from atmospherefunction import atmConditions
 
 """Python 3.7
    EH Group, Inc. (c) 2019
    Christopher Simpson: christopher.r.simpson@simpsonaerospace.com"""
 #------------------------------------------------------------------------------
-#COE of ISS
-rooturl = 'http://www.celestrak.com/NORAD/elements/supplemental/'
-iss_tles = download_file(rooturl+'iss.txt')
-coe = readtle.readtle(iss_tles)
-#a,    semi-major axis [m]
-#e,    eccentricity []
-#i,    inclination [deg]
-#RAAN, right angular ascending node [deg]
-#AOP,  argument of perigee [deg]
-#M,    mean anomaly [deg]
-
-#Launched over Hardaway Hall
-loc_hh = EarthLocation.from_geodetic(-87.545*u.deg,
-                                     33.213*u.deg,
-                                     height=19.25*u.m,
+earthrotspeed = 464.5*u.m/u.s
+#dv_required to reach orbit
+orbit_alt = (np.linspace(250,600,num=7)*u.km).to(u.m) + Earth.R_mean
+orbit_ecc = 0*u.one
+#ToDo: 1500 m/s losses due to drag+gravity should be added here (SPAD)
+orbit_inc = np.tile(np.deg2rad(np.linspace(0,90,num=91)), (len(orbit_alt),1))
+Y_inc, X_alt = np.meshgrid(orbit_inc[0,:], orbit_alt)
+orbit_speed = np.sqrt(Earth.k/X_alt) + 1500*(u.m/u.s)
+#launch site location 
+loc_equator = EarthLocation.from_geodetic(-87.545*u.deg,
+                                     0*u.deg,
+                                     height=(19.25*u.m + 13716*u.m),
                                      ellipsoid='WGS84')
+launch_azI = np.arcsin(np.cos(orbit_inc)/np.cos(0))*u.rad
+launch_sitevelocity = earthrotspeed*np.cos(0)
+launch_correctaz = np.arctan2(launch_sitevelocity*np.cos(launch_azI),(-(earthrotspeed*np.cos(orbit_inc)) + orbit_speed))
+launch_az = launch_correctaz + launch_azI
+#aircraft
+M = np.linspace(0.3,1.6)*u.one
+ac_alt = 45000*u.imperial.ft
+T, p, rho, R_air, g0 = atmConditions(ac_alt)
+ac_speedofsound = ((np.sqrt(1.4*R_air*T)).value)*0.3048*u.m/u.s
+ac_speed = ac_speedofsound*M
+orbit_DV = np.ones((len(orbit_alt),orbit_inc.shape[1],len(ac_speed)))
+for k in range(len(ac_speed)):
+    for i in range(len(orbit_alt)):
+        for j in range(orbit_inc.shape[1]):
+            orbit_target = Orbit.circular(Earth, (orbit_alt[i] - Earth.R_mean), (orbit_inc[i,j]*u.rad).to(u.deg))
+            orbit_target = orbit_target.propagate(47.30*u.s)
+            orbit_fp = fp_angle(orbit_target.nu, orbit_target.ecc)
+            holder = [[(-(orbit_speed[i,j]-ac_speed[k])*np.cos(launch_az[i,j])*np.cos(orbit_fp)).value,
+                      ((orbit_speed[i,j]-ac_speed[k])*np.sin(launch_az[i,j])*np.cos(orbit_fp) - launch_sitevelocity).value,
+                      ((orbit_speed[i,j]-ac_speed[k])*np.sin(orbit_fp)).value]]
+            holder = np.linalg.norm(holder)
+            orbit_DV[i,j,k] = holder
 
-#time
-t_ob = Time(Time.now(),scale='tai')
-delta_t = TimeDelta(np.linspace(0,86400,num=86401)*u.second,scale='tai')
-t_del_nounit = np.linspace(0,86400,num=86401)*u.second
+#missile_DV = aim120ToSpace()
 
-#topocentric frame at Hardaway Hall
-frame_now_hh = AltAz(obstime=t_ob+delta_t,location=loc_hh)
+#plotting
+inc_Y, alt_X = np.meshgrid(np.rad2deg(orbit_inc[0,:]), orbit_alt)
+fig, ax = plt.subplots()
+dv = ax.contourf(alt_X, inc_Y, orbit_DV[:,:,0])
+ax.clabel(dv, inline=1, fontsize=8)
+ax.set_title('$\Delta$V Required to Achieve Orbit')
 
-#Velocity of Earth at equator
-Veq = 464.5 *(u.m/u.s)
-#Earth inertial velocity addition to launch
-Vgs_lat = Veq*np.cos(np.deg2rad(33.213))
-#aircraft speed
-V_ac = (np.array([np.linspace(0.3,1.6,num=14)])).transpose() * u.one #mach number
-
-h_ac = 45000*u.imperial.ft
-Temp, press, density, R_air = atmConditions(h_ac)
-speed_of_sound = np.sqrt(1.4*R_air*Temp)
-V_ac = speed_of_sound*V_ac
-#speed of satellite immediately after launch
-V_sat = V_ac + (7800*u.m/u.s)
-#inertial launch azimuth
-beta_i = np.arcsin(np.cos(np.deg2rad(coe[2,0]))/np.cos(np.deg2rad(33.213)))*u.rad
-#correction to account for Earth rotation velocity addition
-gam_correction = np.arctan2(((Vgs_lat*np.cos(beta_i))+V_ac),(V_sat - (Veq*np.cos(np.deg2rad(coe[2,0])))))
-#launch azimuth (ascending node)
-beta = beta_i + gam_correction
-#local sidereal time
-theta = Longitude(coe[3,0]*u.deg + t_ob.sidereal_time('apparent',longitude=360-87.545))
-
-nu = D_to_nu(coe[5,0]*u.deg)
-iss_target = Orbit.from_classical(Earth, coe[0,0]*u.m,
-                                  coe[1,0]*u.one,
-                                  coe[2,0]*u.deg,
-                                  coe[3,0]*u.deg,
-                                  coe[4,0]*u.deg,
-                                  nu*u.deg,
-                                  epoch = t_ob)
-X = mean_motion(Earth.k, iss_target.r, iss_target.v, t_del_nounit)
-#iss_SkyCoord = SkyCoord(X[0][:,0], X[0][:,1], X[0][:,2], frame = 'gcrs', representation_type='cartesian')
-#ssaltaz_hh = iss_SkyCoord.transform_to(frame_now_hh)
-#iss_target_SEZ = transform(iss_target, 'gcrs', frame_now_hh)
-V_req = np.array([[-np.cos(beta)*X[1][:,0]],[(X[1][:,1]*np.sin(beta))-(Vgs_lat+V_ac)],[0*X[1][:,2]]])
+plt.show()
